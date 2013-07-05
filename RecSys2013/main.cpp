5ac91c4dfe97e5cfe5db5a745cdab4452ceecb86
wcw <hustwcw@qq.com>
2013-07-05 08:47:01 +0800

    Initial Commit

---
+++ RecSys2013/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RecSys2013
+//
+//  Created by JTangWang on 13-7-5.
+//  Copyright (c) 2013年 JTangWang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
