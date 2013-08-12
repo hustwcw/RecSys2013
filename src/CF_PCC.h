6325f8646110ac588a365c22df2fdfec4035a359
wcw <hustwcw@qq.com>
2013-08-12 15:04:08 +0800

    将协同过滤算法单独移动出来

---
+++ src/CF_PCC.h
@@ -0,0 +1,16 @@
+//
+//  CF_PCC.h
+//  RecSys2013
+//
+//  Created by JTangWang on 13-8-12.
+//  Copyright (c) 2013年 JTangWang. All rights reserved.
+//
+
+#ifndef __RecSys2013__CF_PCC__
+#define __RecSys2013__CF_PCC__
+
+#include <iostream>
+
+void TestCFPCC();
+
+#endif /* defined(__RecSys2013__CF_PCC__) */
